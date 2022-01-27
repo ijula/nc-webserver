9863319dd2abc1af8ff2afb4bd72093b4fab2073
Ionut Jula <ionut.jula@emerson.com>
2022-01-27 12:07:01 +0200

    Update nc-webserver.sh

--- nc-webserver.sh
+++ nc-webserver.sh
@@ -4,7 +4,7 @@ declare -r host='127.0.0.1'
 declare -r port='8888'
 
 while true; do
-body=$(cat <<EOF
+body="$(cat <<EOF
 <!doctype html>
 <html>
 <meta http-equiv="refresh" content="1">
@@ -14,14 +14,14 @@ Date: $(date)
 </body>
 </html>
 EOF
-)
+)"
 
-response=$(cat <<EOF
+response="$(cat <<EOF
 HTTP/1.0 200 OK
 
 $body
 EOF
-)
+)"
 
 echo "--- $((x=x+1)) ---"
 echo "$response" | nc -l "$host" "$port"
